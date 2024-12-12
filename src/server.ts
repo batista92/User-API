356ac8ccabdaeba79f5ff1d86bd5508ffeb858ad
batista92 <bruno.2.s@live.com>
2024-12-12 01:49:08 -0300

    chore: register service and repository with container

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import app from './app';
+import container from './container';
+import UserRepository from './repositories/user.repository';
+import UserService from './services/user.service';
 
 const PORT = process.env.PORT || 3000;
+
+container.bind<UserService>(UserService).to(UserService);
+container.bind<UserRepository>(UserRepository).to(UserRepository);
 
 app.listen(PORT, () => [
   console.log('Server running on port 3000!')
